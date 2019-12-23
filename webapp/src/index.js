ed7e7b8e582e55c56023b33f570f6dc4a820d823
Nattharee Pisarnsathukit <penquin501@gmail.com>
2019-12-23 18:47:44 +0700

    update function to event emitter

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -4,7 +4,11 @@ var cron = require('node-cron');
 const app = express();
 const moment = require('moment');
 var m = require('moment-timezone');
+const events = require('events');
+
 const port = process.env.PORT || 3200;
+const bus = new events.EventEmitter();
+
 moment.locale('th')
 app.use(express.json());
 
@@ -12,6 +16,7 @@ const connection = require("./env/db");
 const billing_connection = require("./env/mainDB");
 
 const updateServices = require('./services/updateService.js');
+require('./events/verify.js')(bus);
 
 app.get("/senddata", function (req, res) {
   // res.json({ 'hello': 'World' });
@@ -22,20 +27,23 @@ app.get("/senddata", function (req, res) {
   };
 
   var dataBill = {
-    billing_no: '47-783-191219160646-401'
+    billing_no: '47-783-191221154344-796'
   };
 
   sendDataToMainServerTemp(dataAuthen,dataBill);
 });
+
 function sendDataToMainServerTemp(dataAuthen, dataBill) {
   var orderlist = [];
   var paymentType = "";
 
-  updateServices.selectDataInBillNo(dataBill.billing_no).then(function (data) {
-    console.log(data.length);
+  updateServices.selectDataInBillNo(dataBill.billing_no).then(function (res) {
+    
+    var data=res.billingItem
+    // console.log(data[0]);
     for (i = 0; i < data.length; i++) {
       // "paymenttype": "99",99=normal,60=cod
-      if (data[i].parcel_type.toUpperCase() == "NORMAL") {
+      if (data[i].bi_parcel_type.toUpperCase() == "NORMAL") {
         paymentType = "99";
       } else {
         paymentType = "60";
@@ -44,7 +52,7 @@ function sendDataToMainServerTemp(dataAuthen, dataBill) {
         productinfo: {
           globalproductid: data[i].product_id,
           productname: data[i].product_name,
-          methodtype: data[i].parcel_type.toUpperCase(),
+          methodtype: data[i].bi_parcel_type.toUpperCase(),
           paymenttype: paymentType,
           price: data[i].size_price.toString(),
           codvalue: data[i].cod_value.toString()
@@ -52,7 +60,7 @@ function sendDataToMainServerTemp(dataAuthen, dataBill) {
         destinationinfo: {
           custname: data[i].receiver_name,
           custphone: data[i].phone,
-          custzipcode: data[i].zipcode,
+          custzipcode: data[i].br_zipcode,
           custaddr: data[i].receiver_address,
           //   "custdistrict": data[i].district_name,
           //   "custamphur": data[i].amphur_name,
@@ -74,21 +82,21 @@ function sendDataToMainServerTemp(dataAuthen, dataBill) {
 
     var dataAll = {
       authen: {
-        merid: dataAuthen.branch_id,
-        userid: dataAuthen.user_id,
-        merauthenlevel: dataAuthen.mer_authen_level
+        merid: res.billingInfo[0].branch_id,
+        userid: res.billingInfo[0].user_id,
+        merauthenlevel: res.billingInfo[0].mer_authen_level
       },
       memberparcel: {
         memberinfo: {
-          memberid: data[0].member_code,
-          courierpid: data[0].carrier_id,
-          courierimage: data[0].img_url
+          memberid: res.billingInfo[0].member_code,
+          courierpid: res.billingInfo[0].carrier_id,
+          courierimage: res.billingInfo[0].img_url
         },
-        billingno: dataBill.billing_no,
+        billingno: res.billingInfo[0].billing_no,
         orderlist: orderlist
       }
     };
-    console.log("dataAll", JSON.stringify(dataAll));
+    // console.log("dataAll", JSON.stringify(dataAll));
     request({
         url: "https://www.945holding.com/webservice/restful/parcel/order_record/v11/data",
         method: "POST",
@@ -99,8 +107,8 @@ function sendDataToMainServerTemp(dataAuthen, dataBill) {
           "Content-Type": "application/json"
         }
       },
-      (err, res, body) => {
-        // console.log(res.body);
+      (err, res2, body) => {
+        console.log(res2);
         if (
           res.body.checkpass == "pass" &&
           res.body.bill_no == "data_varidated_pass"
@@ -133,103 +141,19 @@ myFunction = async (t) => {
     await updateServices.selectBillingNotSend().then(function (listBilling) { 
       if(listBilling!==null) {
         for(i=0;i<listBilling.length;i++) {
-          billing=listBilling[i].billing_no
-          // billing='47-1108-191220163458-601'
-          updateServices.selectDataInBillNo(billing).then(function (res) {
-            var orderlist = [];
-            var paymentType = "";
-            let dataBilling=res.billingInfo
-            let data=res.billingItem
-
-            for (j = 0; j < data.length; j++) {
-              if (data[j].parcel_type == "NORMAL") {
-                paymentType = "99";
-              } else {
-                paymentType = "60";
-              }
-              dataDes = {
-                productinfo: {
-                  globalproductid: data[j].product_id,
-                  productname: data[j].product_name,
-                  methodtype: data[j].bi_parcel_type.toUpperCase(),
-                  paymenttype: paymentType,
-                  price: data[j].size_price.toString(),
-                  codvalue: data[j].cod_value.toString()
-                },
-                destinationinfo: {
-                  custname: data[j].receiver_name,
-                  custphone: data[j].phone,
-                  custzipcode: data[j].br_zipcode,
-                  custaddr: data[j].receiver_address,
-                  ordershortnote: '',
-                  districtcode: data[j].DISTRICT_CODE,
-                  amphercode: data[j].AMPHUR_CODE,
-                  provincecode: data[j].PROVINCE_CODE,
-                  geoid: data[j].GEO_ID,
-                  geoname: data[j].GEO_NAME,
-                  sendername: data[j].sender_name,
-                  senderphone: data[j].sender_phone,
-                  senderaddr: data[j].sender_address
-                },
-                consignmentno: data[j].tracking
-              };
-              orderlist.push(dataDes);
-            }
-            
-            var dataAll = {
-              authen: {
-                merid: dataBilling[0].branch_id,
-                userid: dataBilling[0].user_id,
-                merauthenlevel: dataBilling[0].mer_authen_level
-              },
-              memberparcel: {
-                memberinfo: {
-                  memberid: dataBilling[0].member_code,
-                  courierpid: dataBilling[0].carrier_id,
-                  courierimage: dataBilling[0].img_url
-                },
-                billingno: dataBilling[0].billing_no,
-                orderlist: orderlist
-              }
-            }
-            console.log(dataBilling[0].billing_no);
-            // request(
-            //   {
-            //     url:
-            //       "https://dev.945holding.com/webservice/restful/parcel/order_record/v11/data",
-            //     method: "POST",
-            //     body: dataAll,
-            //     json: true,
-            //     headers: {
-            //       'apikey': 'XbOiHrrpH8aQXObcWj69XAom1b0ac5eda2b',
-            //       'Content-Type': 'application/json'
-            //     }
-            //   },
-            //   (err, res, body) => {
-            //     if (res) {
-            //       console.log(res.body);
-            //       if (res.body.checkpass == 'pass' && res.body.bill_no == 'data_varidated_pass') {
-            //         let updateBilling = "UPDATE billing_test SET status='" + res.body.checkpass + "' WHERE billing_no='" + dataBilling[0].billing_no + "'"
-            //         connection.query(updateBilling, function (err, dataBilling) { })
-
-            //         listStatus = res.body.status
-            //         for (q = 0; q < listStatus.length; q++) {
-            //           var tracking = listStatus[q].consignmentno
-            //           var status = listStatus[q].status
-            //           var dateTimeString = moment(new Date).format("YYYY-MM-DD HH:mm:ss", true);
-            //           updateServices.updateStatusReceiverInfo(tracking, status, dateTimeString).then(function (data) { })
-            //         }
-            //       }
-            //     }
-            //   })
-          }) 
+          value={
+            connection: connection,
+            billingNo: listBilling[i].billing_no
+          }
+            bus.emit('set_pending',value);
         }
       }
       
     })
 
   //---------------
-
+  console.log("%s     process return", m().format(t_format));
+  console.log("%s   End execute myFunction(%d)", m().format(t_format), tid);
   sim_execute_time += 500;
 }
 
@@ -369,7 +293,7 @@ function sendDataToMainServer() {
                         var tracking = listStatus[q].consignmentno
                         var status = listStatus[q].status
                         var dateTimeString = moment(new Date).format("YYYY-MM-DD HH:mm:ss", true);
-                        updateServices.updateStatusReceiverInfo(tracking, status, dateTimeString).then(function (data) { })
+                        updateServices.updateStatusReceiverInfo(tracking, status, dateTimeString).then(function (data) {})
                       }
                     }
                   }
--- webapp/src/services/updateService.js
+++ webapp/src/services/updateService.js
@@ -2,11 +2,15 @@ const connection = require('../env/db.js');
 const bodyParser = require('body-parser');
 const request = require('request');
 const moment = require('moment');
+
+const events = require('events');
+const bus2 = new events.EventEmitter();
+
+require('../events/format.js')(bus2);
+
 moment.locale('th')
 
 module.exports = {
-
-
     updateStatusReceiverInfo: (tracking, status, dateTimeString) => {
         return new Promise(function (resolve, reject) {
             let sql = "SELECT status FROM billing_receiver_info where tracking='" + tracking + "'"
@@ -20,7 +24,7 @@ module.exports = {
         })
     },
     selectBillingNotSend: () => {
-        var sqlBillingNotSend = "SELECT billing_no FROM billing_test WHERE status is null";
+        var sqlBillingNotSend = "SELECT billing_no FROM billing WHERE status is null";
         return new Promise(function (resolve, reject) {
             connection.query(sqlBillingNotSend, (error, results, fields) => {
                 resolve(results);
@@ -28,7 +32,6 @@ module.exports = {
         })
     },
     selectDataInBillNo: (bill_no) => {
-        // console.log(bill_no);
         var sqlBilling = "SELECT user_id,mer_authen_level,member_code,carrier_id,billing_no,branch_id,total,img_url FROM billing WHERE billing_no= ?"
         var dataBilling = [bill_no];
 
@@ -52,40 +55,49 @@ module.exports = {
                     resolve(false)
                 } else {
                     connection.query(sqlBillingItem, dataBillItem, (err, resultBillingItem) => {
-                        if (!resultBillingItem) {
-                            resolve(false)
-                        } else {
-                            var check_pass
-                            for (i = 0; i < resultBillingItem.length; i++) {
-                                if(!resultBilling[0].user_id ||!resultBilling[0].mer_authen_level ||!resultBilling[0].member_code||
-                                    !resultBilling[0].carrier_id||!resultBilling[0].billing_no||!resultBilling[0].branch_id){
-                                    check_pass = false;
-                                } else if (!resultBillingItem[i].bi_parcel_type || !resultBillingItem[i].size_price || !resultBillingItem[i].receiver_name ||
-                                    !resultBillingItem[i].phone || !resultBillingItem[i].receiver_address || !resultBillingItem[i].DISTRICT_CODE ||
-                                    !resultBillingItem[i].AMPHUR_CODE || !resultBillingItem[i].PROVINCE_CODE || !resultBillingItem[i].br_zipcode ||
-                                    !resultBillingItem[i].product_id || !resultBillingItem[i].product_name || !resultBillingItem[i].GEO_ID || !resultBillingItem[i].tracking
-                                ) {
-                                    check_pass = false;
-                                } else if(resultBillingItem[i].bi_parcel_type!=resultBillingItem[i].br_parcel_type || resultBillingItem[i].bi_zipcode!=resultBillingItem[i].br_zipcode){
-                                    check_pass = false;
-                                } else {
-                                    check_pass = true;
-                                }
-                            }
-                            if (check_pass) {
+                        // if (!resultBillingItem) {
+                        //     resolve(false)
+                        // } else {
+                        //     var check_pass
+                        //     for (i = 0; i < resultBillingItem.length; i++) {
+                        //         if(!resultBilling[0].user_id ||!resultBilling[0].mer_authen_level ||!resultBilling[0].member_code||
+                        //             !resultBilling[0].carrier_id||!resultBilling[0].billing_no||!resultBilling[0].branch_id){
+                        //             check_pass = false;
+                        //         } else if (!resultBillingItem[i].bi_parcel_type || !resultBillingItem[i].size_price || !resultBillingItem[i].receiver_name ||
+                        //             !resultBillingItem[i].phone || !resultBillingItem[i].receiver_address || !resultBillingItem[i].DISTRICT_CODE ||
+                        //             !resultBillingItem[i].AMPHUR_CODE || !resultBillingItem[i].PROVINCE_CODE || !resultBillingItem[i].br_zipcode ||
+                        //             !resultBillingItem[i].product_id || !resultBillingItem[i].product_name || !resultBillingItem[i].GEO_ID || !resultBillingItem[i].tracking
+                        //         ) {
+                        //             check_pass = false;
+                        //         } else if(resultBillingItem[i].bi_parcel_type!=resultBillingItem[i].br_parcel_type || resultBillingItem[i].bi_zipcode!=resultBillingItem[i].br_zipcode){
+                        //             check_pass = false;
+                        //         } else {
+                        //             check_pass = true;
+                        //         }
+                        //     }
+                        //     if (check_pass) {
                                 var dataResult = {
                                     billingInfo: resultBilling,
                                     billingItem: resultBillingItem
                                 }
-                                resolve(dataResult);
-                            }
-                        }
+                        //         resolve(dataResult);
+                        //     }
+                        // }
+                        resolve(dataResult);
                     })
                 }
             });
         });
     },
-
+    updatePending: (status,billingNo) => {
+        let updateBilling = "UPDATE billing SET status=? WHERE billing_no=?"
+        let data=[status,billingNo];
+        return new Promise(function (resolve, reject) {
+            connection.query(updateBilling, data, (err, results) => {
+                bus2.emit("verify", billingNo);
+            });
+        });
+    },
     updateStatusBilling: (bill_no, status) => {
         let sql = "UPDATE billing SET status=? WHERE billing_no=?"
         var data = [status, bill_no]
